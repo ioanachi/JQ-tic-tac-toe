d8c451443310ae0471b74253227e42d5d7a52755
ioanachi <ioana_chichi@outlook.com>
2017-10-03 14:50:25 +0300

    last

--- Version 2/src/js/script.js
+++ Version 2/src/js/script.js
@@ -10,22 +10,18 @@ $("document").ready(function() {
   vb2 = $(".b2").html();
   vb3 = $(".b3").html();
   vc1 = $(".c1").html();
-  vc2= $(".c2").html();
+  vc2 = $(".c2").html();
   vc3 = $(".c3").html();
 }
   function combinationarr (){
   var arr =[[va1,va2,va3], [vb1,vb2,vb3],[vc1,vc2,vc3],[va1,vb1,vc1], [va2,vb2,vc2],[va3,vb3,vc3], [va1,vb2,vc3], [va3,vb2,vc1]];
   console.log(arr);
-
-
 // i in interiorul lui arr
 for (var i = 0; i < arr.length; i++) {
-  // z in interiorul arrayurilor multiple
-
+  // z in interiorul arrayurilor
     var one = arr[i][0];
     var two = arr[i][1];
     var three = arr[i][2];
-
     if (one == "X" && three == "X" && two == "X") {
       $("h2").html(" X wins");
     }
@@ -36,21 +32,8 @@ for (var i = 0; i < arr.length; i++) {
     //   $("h2").html(" it's a  tie");
     //
     // }
-
 }
-
-
 }
-
-
-
-
-
-
-
-
-
-
   var isx = true;
   $("td").click(function() {
     vales();
